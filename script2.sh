96c3649d5c8f09a4f6df7365434bba0123d9c85f
eduvelasquez110-wq <eduvelasquez110@gmail.com>
2025-12-03 15:56:38 +0000

    Mi primer Linux

---
+++ script2.sh
@@ -0,0 +1,4 @@
+uname -a    # Información sobre el sistema operativo y la plataforma de hardware de una máquina Linux (-a  solicita mostrar toda la información disponible como el nombre del kernel, el nombre del nodo , la versión del kernel, la fecha de compilación del kernel, la arquitectura de la máquina y el sistema operativo)
+lscpu  # Información detallada sobre la configuración de la CPU
+free -h   # Verificar la memoria RAM en su sistema o verificar las estadísticas de memoria del sistema operativo Linux  (-h  Para que nos de la misma información en Gb, Mb y Kb)
+df -h  # Información de espacio en disco en unidad de 1Kb  (-h  Para que nos de la misma información en Gb, Mb y Kb)
