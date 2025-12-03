abaa118fc694b55d78e6cbf08a8308a580018bde
eduvelasquez110-wq <eduvelasquez110@gmail.com>
2025-12-03 15:41:35 +0000

    Mi primer scrip en Linux

---
+++ script.sh
@@ -0,0 +1,8 @@
+
+#Crea y listar archivos 
+echo "Hola Linux" > archivo.txt   #Para mostrar un mensaje de varias líneas sin mostrar ningún comando
+cat archivo.txt   #Permite crear, fusionar o imprimir archivos en la pantalla de salida estándar o en otro archivo
+cp archivo.txt copia.txt   #Copiar uno o más archivos a una ubicación especificada por el usuario
+ls -l   #Listar archivos o directorios desde la CLI 
+rm archivo.txt   #Para eliminar archivos o directorios completos
+
